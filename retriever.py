62fdd411a6779b2c190ff1103fda2dd35766d911
Bell Eapen <github_public@gulfdoctor.net>
2024-11-26 11:01:24 -0600

    Add retriever module for context-based question answering using Google Generative AI

---
+++ retriever.py
@@ -0,0 +1,29 @@
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.retrievers import PubMedRetriever
+
+
+template = """Answer the question based only on the following context:
+
+{context}
+
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+model = GoogleGenerativeAI(model="models/gemini-1.5-flash-latest")
+retriever = PubMedRetriever()
+
+def format_docs(docs):
+    return "\n\n".join([d.page_content for d in docs])
+
+
+chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | model
+    | StrOutputParser()
+)
+
+chain.invoke("What is the latest treatment for ataxia telangiectasia?")
