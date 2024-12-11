ab55150d90f5a51d220c56f64ca2df4ad5b1bc84
Bell Eapen <github_public@gulfdoctor.net>
2024-12-10 20:54:21 -0600

    Add MPS support check for Mac in GPU status script

--- check_gpu.py
+++ check_gpu.py
@@ -17,3 +17,15 @@ if torch.cuda.is_available():
 
 else:
     print("CUDA is not available")
+
+# check if mps is available on Mac
+if torch.backends.mps.is_available():
+    print("MPS is available")
+    num_devices = torch.mps.device_count()
+    print("Number of MPS devices:", num_devices)
+    allocated_mem = torch.mps.current_allocated_memory()
+    print("Current MPS memory usage:", allocated_mem / 1024**2, "MB")
+    driver_mem = torch.mps.driver_allocated_memory()
+    print("Current MPS driver memory usage:", driver_mem / 1024**2, "MB")
+    recommended_mem = torch.mps.recommended_max_memory()
+    print("Recommended MPS memory usage:", recommended_mem / 1024**2, "MB")
